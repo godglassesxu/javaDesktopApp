08e4ac25b072c7c259b10c47821521f6ff2a6fe3
xu <godglasses.xu@gmail.com>
2024-08-25 00:05:42 +0800

    INIT

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.xu.javadesktopapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.xu.javadesktopapp to javafx.fxml;
+    exports com.xu.javadesktopapp;
+}
